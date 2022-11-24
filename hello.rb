0e6541bdcf665c3cd5093c229ef58b2428b5454c
Andy Waite <andyw8@users.noreply.github.com>
2022-11-24 18:04:45 -0500

    Comment out binding.break calls to test VS Code extension

--- hello.rb
+++ hello.rb
@@ -2,8 +2,8 @@ Bundler.require
 
 a = 1
 b = 2
-binding.break                          # Program will stop here
+# binding.break                          # Program will stop here
 c = 3
 d = 4
-binding.break                          # Program will stop here
+# binding.break                          # Program will stop here
 p [a, b, c, d]
